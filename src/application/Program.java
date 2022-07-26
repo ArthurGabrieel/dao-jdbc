d41ab4018c690df23edc1a0b391cab27c088f440
Arthur Gabriel <arthurelgg@gmail.com>
2022-07-26 16:41:04 -0300

    Implementing findById

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,19 +2,14 @@ package application;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
-
-import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
 
-        Department obj = new Department(1, "Books");
-
-        Seller seller = new Seller(13, "Arthur", "arthur@gmail.com", new Date(), 5000.0, obj);
-
         SellerDao sellerDao = DaoFactory.createSellerDao();
+
+        Seller seller = sellerDao.findById(7);
 
         System.out.println(seller);
     }
